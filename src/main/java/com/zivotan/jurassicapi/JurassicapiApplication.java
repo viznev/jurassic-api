6ef9ecf7c211bbbef2f90766eef9ea6d1d3786cc
viznev <viznev@users.noreply.github.com>
2020-05-26 23:04:10 -0500

    initial setup

---
+++ src/main/java/com/zivotan/jurassicapi/JurassicapiApplication.java
@@ -0,0 +1,13 @@
+package com.zivotan.jurassicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JurassicapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JurassicapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zivotan/jurassicapi/JurassicapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zivotan.jurassicapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JurassicapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
